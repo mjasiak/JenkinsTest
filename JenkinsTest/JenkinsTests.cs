f3013ce56e0532696fc61c838c5094aa4445941b
Michal Jasiak <michal.jasiak@pl.abb.com>
2018-04-16 08:45:13 +0200

    Initial

---
+++ JenkinsTest/JenkinsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JenkinsTest
+{
+    [TestClass]
+    public class JenkinsTests
+    {
+        [TestMethod]
+        public void JenkinsTestPassed()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+        [TestMethod]
+        public void JenkinsTestFailed()
+        {
+            Assert.IsTrue(1 == 2);
+        }
+    }
+}
